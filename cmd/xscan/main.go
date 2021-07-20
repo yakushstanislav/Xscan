f69958231be8b9dbb64f27936990826eb922d2eb
Stanislav Yakush <st.yakush@yandex.ru>
2021-07-21 00:01:03 +0300

    Print output in async mode and remove verbose flag

--- cmd/xscan/main.go
+++ cmd/xscan/main.go
@@ -15,7 +15,6 @@ var startPort uint64
 var endPort uint64
 var timeout time.Duration
 var workers uint64
-var verbose bool
 
 func parseFlags() {
 	flag.StringVar(&address, "address", "127.0.0.1", "Address")
@@ -23,7 +22,6 @@ func parseFlags() {
 	flag.Uint64Var(&endPort, "end", 65535, "End port range")
 	flag.DurationVar(&timeout, "timeout", time.Second, "Timeout")
 	flag.Uint64Var(&workers, "workers", 1, "Workers")
-	flag.BoolVar(&verbose, "verbose", false, "Verbose")
 
 	flag.Parse()
 }
@@ -37,7 +35,6 @@ func showFlags() {
 	fmt.Printf("> Port range: %d-%d\n", startPort, endPort)
 	fmt.Println("> Timeout:", timeout)
 	fmt.Println("> Workers:", workers)
-	fmt.Println("> Verbose:", verbose)
 
 	fmt.Println("=========================================================")
 }
@@ -49,26 +46,32 @@ func main() {
 	var wg sync.WaitGroup
 
 	addresses := make(chan string)
+	results := make(chan string)
+
+	done := make(chan bool)
+
+	go func() {
+		for result := range results {
+			fmt.Println("Open:", result)
+		}
+
+		done <- true
+	}()
 
 	for i := uint64(0); i <= workers; i++ {
 		wg.Add(1)
 
-		go func(addresses <-chan string) {
+		go func(addresses <-chan string, results chan<- string) {
 			defer wg.Done()
 
 			for address := range addresses {
 				_, err := net.DialTimeout("tcp", address, timeout)
 
 				if err == nil {
-					fmt.Println("Open:", address)
-					continue
-				}
-
-				if verbose {
-					fmt.Println("Closed:", address)
+					results <- address
 				}
 			}
-		}(addresses)
+		}(addresses, results)
 	}
 
 	for i := startPort; i <= endPort; i++ {
@@ -78,4 +81,8 @@ func main() {
 	close(addresses)
 
 	wg.Wait()
+
+	close(results)
+
+	<-done
 }
